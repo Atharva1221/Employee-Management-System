4d28fb530725c2d95c1b24011f76fe98004d636f
Atharva1221 <pachkhedeatharva607@gmail.com>
2025-10-18 17:20:03 +0530

    Initial commit

---
+++ SpringBootLearning/src/main/java/SpringBootLearning/Hello.java
@@ -0,0 +1,13 @@
+package SpringBootLearning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping
+    public String Hello(){
+        return "Hello to everyone involved";
+    }
+
+}
